4eea577c0b3874352a9f031ef23166fed409d980
Naod Taddese <naodtds@gmail.com>
2024-06-18 21:09:31 +0300

    task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+students in collection by ave
+'''
+
+
+def top_students(mongo_collection):
+    '''printed with average score'''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
