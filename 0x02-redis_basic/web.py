a017dbbfb01f31195abf38484d5fc135fe59d172
Naod Taddese <naodtds@gmail.com>
2024-06-19 22:01:23 +0300

    task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+web cache
+"""
+
+
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
+
+
+def url_access_count(method):
+    """get page"""
+    @wraps(method)
+    def wrapper(url):
+        """decorator function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@url_access_count
+def get_page(url: str) -> str:
+    """get html"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
